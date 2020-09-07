61f3c0f6d1bbd95bf20a02484a8649a82447e15f
Hector Perez <hecpeare@gmail.com>
2020-09-07 13:22:58 +0200

    fetch count

--- content.js
+++ content.js
@@ -1,1 +1,6 @@
-alert(`Hello! ${window.location.toString()}`);
+const url = window.location.toString()
+// alert(`Hello! ${url}`);
+
+fetch(`https://935f3a7d7cf4.ngrok.io/v1/search/count?url=${url}`).then(r => r.text()).then(result => {
+  alert(`response: ${result}`)
+})

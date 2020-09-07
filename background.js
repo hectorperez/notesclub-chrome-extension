e84ae7d20dfee07f12c43fd972102a6dfe95f2ee
Hector Perez <hecpeare@gmail.com>
2020-09-07 15:19:22 +0200

    get url, fetch count and display it in the icon

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 var selectedId = -1;
 function refreshCounter() {
-  chrome.browserAction.setBadgeText({ "text": "3", tabId: selectedId });
+  chrome.tabs.query({lastFocusedWindow: true, active: true}, function (tabs) {
+    const url = tabs[0].url;
+    fetch(`https://935f3a7d7cf4.ngrok.io/v1/search/count?url=${url}`).then(r => r.text()).then(result => {
+      chrome.browserAction.setBadgeText({ "text": result, tabId: selectedId });
+    })
+  });
 }
 
 chrome.tabs.onUpdated.addListener(function (tabId, props) {
@@ -17,11 +22,3 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   selectedId = tabs[0].id;
   refreshCounter();
 });
-
-
-// const url = window.location.toString()
-// alert(`Hello! ${url}`);
-
-// fetch(`https://935f3a7d7cf4.ngrok.io/v1/search/count?url=${url}`).then(r => r.text()).then(result => {
-//   alert(`response: ${result}`)
-// })
